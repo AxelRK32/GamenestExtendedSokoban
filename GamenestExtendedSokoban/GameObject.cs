6bbe1a6574ad0dd7f9eb13ff7aa88d820f84923f
AxelRK32 <114733972+AxelRK32@users.noreply.github.com>
2022-10-19 16:19:12 +0200

    Copied over code from old project

---
+++ GamenestExtendedSokoban/GameObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamenestExtendedSokoban
+{
+    class GameObject
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public string mapMarker { get; set; }
+
+        public GameObject(int x, int y, string mapMarker)
+        {
+            X = x;
+            Y = y;
+            this.mapMarker = mapMarker;
+        }
+    }
+}
---
+++ GamenestExtendedSokoban/Map.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamenestExtendedSokoban
+{
+    class Map
+    {
+        string[][] map =
+        {
+            new string[] { "w", "w", "w", "w", "w", "w", "w", "w", "w", "w" },
+            new string[] { "w", "o", ".", ".", "w", ".", ".", ".", ".", "w" },
+            new string[] { "w", ".", ".", ".", "w", ".", "w", "w", ".", "w" },
+            new string[] { "w", ".", "w", ".", "w", ".", "w", "w", ".", "w" },
+            new string[] { "w", ".", "w", ".", "w", ".", "w", "w", "B", "w" },
+            new string[] { "w", ".", ".", ".", "B", ".", ".", ".", ".", "w" },
+            new string[] { "w", ".", "w", ".", "w", ".", "w", "w", ".", "w" },
+            new string[] { "w", ".", "B", ".", "w", "B", "w", "w", "o", "w" },
+            new string[] { "w", ".", ".", ".", "o", ".", "w", "w", "o", "w" },
+            new string[] { "w", "w", "w", "w", "w", "w", "w", "w", "w", "w" }
+        };
+
+        string[][] tempMap;
+
+        public Map()
+        {
+
+        }
+
+        // This function places all my moveable objects on my static map,
+        // then prints it.
+        public void PrintMap(List<GameObject> objects)
+        {
+            tempMap = (string[][])map.Clone();
+            foreach (GameObject obj in objects)
+            {
+                tempMap[obj.X][obj.Y] = obj.mapMarker;
+            }
+
+            PrintMap(tempMap);
+        }
+
+        // This function prints a map that is given to it.
+        private void PrintMap(string[][] mapToPrint)
+        {
+            for (int y = 0; y < mapToPrint.Length; y++)
+            {
+                for (int x = 0; x < mapToPrint[y].Length; x++)
+                {
+                    Console.Write(mapToPrint[y][x] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // Returns what kind of tile it is via an x and y value.
+        public string ReturnTile(int x, int y)
+        {
+            if (x < 0 || y < 0 || x > map[y].Length || y > map.Length)
+            {
+                return "";
+            }
+            return map[y][x];
+        }
+    }
+}
